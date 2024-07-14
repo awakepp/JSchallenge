80a0449357a8efaa71086c874d73485fbc96d228
awakepp <priyanshtiwari1710@gmail.com>
2024-07-14 13:33:32 +0530

    first commit

---
+++ Day1/Day1_1.js
@@ -0,0 +1,47 @@
+//DAY 1 - Variables and Data Types.
+
+//Activity 1 : Variable Declaration
+
+//Task 1
+var num = 1;
+console.log(num);
+//Task 2
+let jusBetter = "Priyansh";
+console.log(jusBetter);
+
+//Activity 2 : Constant Declaration
+
+//Task 3
+const binaryVal = true;
+console.log(binaryVal);
+
+//Activity 3 : Data Types
+
+//Task 4
+const num1 = 11;
+const string1 = "Ascend";
+const bool1 = true;
+const obj1 = {name : "Billy"};
+const arr1 = [1, 2, 3];
+
+console.log(typeof(num1));
+console.log(typeof(string1));
+console.log(typeof(bool1));
+console.log(typeof(obj1));
+console.log(typeof(arr1));
+
+//Activity 4 - Reassigning Variables.
+
+//Task 5
+let value1 = "Wassup";
+console.log(value1);
+value1 = "Mate";
+console.log(value1);
+
+//Activity 5 - Understanding Const.
+
+//Task 6
+const value2 = "Move it";
+console.log(value2);
+value2 = "down";
+console.log(value2);
---
+++ Day1/Day1_2.js
@@ -0,0 +1,36 @@
+//Feature Request :
+
+//Variable type console log
+const num1 = 11;
+const string1 = "Ascend";
+const bool1 = true;
+const obj1 = {name : "Billy"};
+const arr1 = [1, 2, 3];
+
+console.log(num1);
+console.log(string1);
+console.log(bool1);
+console.log(obj1);
+console.log(arr1);
+
+console.log(typeof(num1));
+console.log(typeof(string1));
+console.log(typeof(bool1));
+console.log(typeof(obj1));
+console.log(typeof(arr1));
+
+//Reassignment Demo
+
+//let declaration does let you change its value after assignment.
+
+let check1 = "hi";
+console.log(check1);
+check1 = "hello";
+console.log(check1);
+
+//const doesn't let variable change its value after the assignment.
+
+const check2 = "namaste";
+console.log(check2);
+check2 = "namaskar";
+console.log(check2);
